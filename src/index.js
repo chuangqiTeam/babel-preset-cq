c66fc31e6270d3ca597b408b3cce06517f30a5d0
rainie <463103063@qq.com>
2017-03-23 11:04:49 +0800

    📦  [refact] 添加 modules和env选项

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
 
 import presetLatest from 'babel-preset-latest';
 import presetEnv from 'babel-preset-env';
+import resetReact from 'babel-preset-react';
 
  // class { handleClick = () => { } }
 import transformClassProperties from 'babel-plugin-transform-class-properties';
@@ -15,17 +16,24 @@ import transformObjectRestSpread from 'babel-plugin-transform-object-rest-spread
 import transformRegenerator from 'babel-plugin-transform-regenerator';
 // Polyfills the runtime needed for async/await and generators
 import transformRuntime from 'babel-plugin-transform-runtime';
+// Adds component stack to warning messages
+import transformReactJsxSource from 'babel-plugin-transform-react-jsx-source';
+// Adds __self attribute to JSX which React will use for some warnings
+import transformReactJsxSelf from 'babel-plugin-transform-react-jsx-self';
 
 import path from 'path';
 
-export default function(context, opts = {target: 'web'}) {
+export default function(context, opts = {target: 'web', modules: false, env: 'development'}) {
     const options = opts;
 
     return {
         presets: [
-            options.target === 'web' && [presetLatest, {es2015: {modules: false}}],
+            options.target === 'web' && [presetLatest, {es2015: {modules: options.modules}}],
             // ES features necessary for user's Node version
             options.target === 'node' && [presetEnv, {targets: {node: parseFloat(process.versions.node)}}],
+
+            resetReact,
+
         ].filter(Boolean),
         plugins: [
             transformClassProperties,
@@ -39,6 +47,9 @@ export default function(context, opts = {target: 'web'}) {
                 // Resolve the Babel runtime relative to the config.
                 moduleName: path.dirname(require.resolve('babel-runtime/package')),
             }],
-        ],
+
+            options.env === 'development' && transformReactJsxSource,
+            options.env === 'development' && transformReactJsxSelf,
+        ].filter(Boolean),
     };
 };
